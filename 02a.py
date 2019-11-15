0adb5cc961d0913e7eacce5468553de2075acdea
PD88 <57749073+DzwilewskiPiotr@users.noreply.github.com>
2019-11-15 13:36:04 +0100

    Add files via upload

---
+++ 02a.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+
+print("podaj wage")
+waga =float(input())
+print("podaj wzrost")
+wzrost =float(input())
+print("podaj wiek")
+wiek =float(input())
+print("Jeżeli jesteś kobietą napisz TAK")
+male =input()
+print (male)
+print("Jeżeli jesteś facetem napisz TAK")
+female =input()
+print(female)
+BMR=10*waga+6.25*wzrost-5*wiek+5
+print(BMR*1.2,"kcal")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
